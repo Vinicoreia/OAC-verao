c1083f6274472e3a79ae3dcbcaa3a82c119ac021
x04618860146 <x04618860146@tcu.gov.br>
2017-01-06 15:07:21 -0200

    adicionando escopo dos metodos principais

--- trabalho1.asm
+++ trabalho1.asm
@@ -14,10 +14,36 @@
 	tab: .asciiz "	"
 	enter: .asciiz "\n"
 	comma: .asciiz ","
-	matrizA: .space 72 # Espaco na memoria para manter a matriz A
+	matrizD: .space 256 # Espaco na memoria para manter a matriz A
+	matrizS: .space 256 # Espaco na memoria para manter a matriz A
+	matrizT: .space 256 # Espaco na memoria para manter a matriz A
+	doubleZero: .double 0.0 # Constante 0 com precisao dupla
+	doubleUm: .double 1.0 # Constante 1 com precisao dupla
+	
+	mensagemMatrizD: .asciiz "\n\nA matriz Destino é : \n"
+	mensagemMatrizS: .asciiz "\n\nA matriz 1 é : \n"
+	mensagemMatrizT: .asciiz "\n\nA matriz 2 é : \n" # Analogia a rs, rt e rd
+	mensagemDimensao: .asciiz "Digite a dimensao n da matriz \n	Ex: caso digite 3 será criada uma matriz quadrada de 3 linhas e 3 colunas"
 .text
 
 main: 
 	la $a0, mensagemInicial
 	li $v0, 4
+	syscall
+	
+	ldc1 $f28, doubleZero # carrega em f28 constante zero precisao dupla
+	ldc1 $f30,doubleUm # carrega em f30 constante um precisao dupla
+	jal getMatrixes
+	j exit
+getMatrixes:
+	la $a0, mensagemDimensao
+	li $v0, 4
+	syscall
+	
+	jr $ra
+
+somaMatriz:
+
+exit: 
+	li $v0, 10
 	syscall
